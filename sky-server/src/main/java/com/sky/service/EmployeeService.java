c9a7eab443bd883178f1ef8a8e3ec5166a839aff
。 <cyyy2510@gmail.com>
2026-07-30 02:18:29 +0800

    启用禁用员工账号业务代码

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -30,4 +30,10 @@ public interface EmployeeService {
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, long id);
 }
